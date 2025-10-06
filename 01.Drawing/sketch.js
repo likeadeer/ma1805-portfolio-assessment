c68a93c8de26593e6be954244b369506d73af284
abi <abiantrobus444@icloud.com>
2025-10-06 14:58:11 +0100

    edit

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -1,8 +1,10 @@
 function setup() {
   createCanvas(400, 400);
+  background (235, 199, 237)
 }
 
 function draw() {
-  background(220);
-  circle(50,50,50)
+  //background(220);
+  triangle (250, 350, 350, 200, 450, 350)
+  fill (204, 121, 94)
 }
